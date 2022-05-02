99fafeae8bd7f3d9c8d305cc9ad90b058528df9c
Richard <cguo3889@uni.sydney.edu.au>
2022-05-03 06:37:46 +1000

    add

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dbConnect = require('./dbConnect');
+const app = express();
+app.use(express.json());
+const newsRoute = require('./routes/newsRoute')
+const userRoute = require('./routes/userRoute')
+
+const port = process.env.PORT || 8000
+
+
+app.use('/api/newsitems/', newsRoute)
+app.use('/api/users/', userRoute)
+
+const path = require('path');
+if(process.env.NODE_ENV === 'production'){
+    app.use('/', express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,'client/build/index.html'))
+    })
+}
+
+app.get('/', (req, res) => res.send("hello world"));
+app.listen(port, () => console.log(`Example app listening port on ${port}`));
